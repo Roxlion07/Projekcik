67ec99f4c1a1fab656b127419b8cce2d3ae59100
Roxlion07 <157325512+Roxlion07@users.noreply.github.com>
2024-01-22 17:31:18 +0100

    Update kalkulator.js

--- kalkulator.js
+++ kalkulator.js
@@ -9,6 +9,9 @@ function czysc(){
 function wynik(){
     try{
         document.getElementById('wpis').value = eval(document.getElementById('wpis').value);
+        if(document.getElementById('wpis').value=="Infinity"){
+            document.getElementById('wpis').value="Nie dziel przez zero!";
+        }
     }
     catch(error){
         document.getElementById('wpis').value = "Błąd";
